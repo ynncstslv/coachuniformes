77ba446ebe6fea8a08fdfee74808fea43df51f00
ynncstslv <ynncstslv@gmail.com>
2023-11-10 12:13:59 -0300

    fix: redefining landing route structure

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,11 @@
+export default function LandingLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className="">{children}</body>
+		</html>
+	);
+}
