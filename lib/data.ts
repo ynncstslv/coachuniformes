f74b52010b2667895e3b5e1281f1f0bfd8c1ba24
ynncstslv <ynncstslv@gmail.com>
2023-11-10 12:11:32 -0300

    feat: data file to map over

---
+++ lib/data.ts
@@ -0,0 +1,103 @@
+export const navRoutes = [
+	{
+		id: 1,
+		name: 'Home',
+		route: '/',
+	},
+	{
+		id: 2,
+		name: 'Quem Somos',
+		route: '/about',
+	},
+	{
+		id: 3,
+		name: 'Produtos',
+		route: '/produtos',
+	},
+	{
+		id: 4,
+		name: 'Contato',
+		route: '/contato',
+	},
+] as const;
+
+export const services = [
+	{
+		id: 1,
+		icon: '',
+		name: 'Roupas Profissionais',
+		description:
+			'Escolha entre uma variedade de uniformes sociais e profissionais que refletem a essência da sua empresa.',
+	},
+	{
+		id: 2,
+		icon: '',
+		name: 'Camisetas Promocionais',
+		description:
+			'Camisetas personalizadas para seus eventos ou uso diário, em malhas e tecidos que destacam sua marca.',
+	},
+	{
+		id: 3,
+		icon: '',
+		name: 'Uniformes Escolares',
+		description:
+			'Do uniforme diário às camisas dos jogos, oferecemos a identidade que sua escola merece.',
+	},
+	{
+		id: 4,
+		icon: '',
+		name: 'Impressão em Serigrafia',
+		description:
+			'Impressões de alta qualidade para estampar suas camisetas e destacar sua mensagem.',
+	},
+	{
+		id: 5,
+		icon: '',
+		name: 'Impressão Digital',
+		description:
+			'Impressões digitais para suas fotos favoritas ou designs exclusivos que cobrem toda a camiseta.',
+	},
+	{
+		id: 6,
+		icon: '',
+		name: 'Bordados',
+		description:
+			'Personalize seus uniformes com o bordado da logo da sua empresa, elevando sua presença de forma elegante.',
+	},
+] as const;
+
+export const testimonials = [
+	{
+		id: 1,
+		name: 'Jilvon Barros',
+		text: 'Excelente atendimento, pontualidade e confiabilidade totais por um preço honesto.',
+	},
+	{
+		id: 2,
+		name: 'Bruno Henrique',
+		text: 'Qualidade e excelência! Já fiz trabalhos com a Coach e o resultado foi muito bom.',
+	},
+	{
+		id: 3,
+		name: 'Wilson Pacheco',
+		text: 'Empresa comprometida com a qualidade de seus produtos e com a satisfação do cliente!',
+	},
+	{
+		id: 4,
+		name: 'New Energy Solar',
+		text: 'Excelente atendimento. Produtos de alta qualidade.',
+	},
+] as const;
+
+export const footerRoutes = [
+	{
+		id: 1,
+		name: 'FAQ',
+		route: '/faq',
+	},
+	{
+		id: 2,
+		name: 'Trabalhe Conosco',
+		route: '/jobs',
+	},
+] as const;
